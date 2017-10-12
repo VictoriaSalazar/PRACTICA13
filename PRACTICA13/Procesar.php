b8e92e2ecc1f2a098371f7e7b3da24e75f7ce7b6
VictoriaSalazar <30777547+VictoriaSalazar@users.noreply.github.com>
2017-10-11 23:35:56 -0500

    Add files via upload

---
+++ PRACTICA13/Procesar.php
@@ -0,0 +1,7 @@
+<?php
+$Nombre=$_POST['NombreAlumno'];
+$Apellidos=$_POST['ApellidosAlumno'];
+echo 'TU NOMBRE ES:'.$Nombre.' '.$Apellidos.'<br>';
+$Edad=$_POST['EdadAlumno'];
+echo 'TU EDAD ES:'.$Edad;
+?>
---
+++ PRACTICA13/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="Procesar.php" method="post" name="DatosPersonales">
+  Nombre:<br>
+  <input type="text" name="NombreAlumno" value="" placeholder="Introducir nombre">
+  <br><br>
+  Apellidos:<br>
+  <input type="text" name="ApellidosAlumno" value="" placeholder="Introducir apellido">
+  <br><br>
+   Edad:<br>
+  <input type="text" name="EdadAlumno" value="">
+  <br><br>
+  <input type="submit" value="Guardar">
+</form> 
+
+<p>Victoria Alejandra Jimenez Salazar</p>
+
+</body>
+</html>
